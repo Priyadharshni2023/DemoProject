ae84b1a1e2a2854abeaf24e2327efee2a9f98e09
Priyadharshni2023 <rpd15996@gmail.com>
2023-08-14 13:21:10 +0200

    Initial commit - Code update II

--- src/test/java/qa/pages/HomePage.java
+++ src/test/java/qa/pages/HomePage.java
@@ -7,8 +7,11 @@ import org.openqa.selenium.support.PageFactory;
 import static qa.base.Base.driver;
 
 public class HomePage {
+    // This element is used to locate the Accept all cookie button in Google
     public By CookiesAcceptAllButton = By.xpath("//*[@id=\"L2AGLb\"]/div");
+    // This element is to locate the search bar in google home
     public By InputField = By.id("APjFqb");
+    //This element is to find the first search result in results page
     public By SearchResult = By.xpath("//h3[contains(@class,'LC20lb')]");
     public HomePage(WebDriver driver)
     {
